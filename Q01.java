088213a41ea91aa98e4c18aeb84b757035e82265
hello1sa <91680305+hello1sa@users.noreply.github.com>
2021-11-23 11:29:13 -0300

    Add files via upload

---
+++ Q01.java
@@ -0,0 +1,31 @@
+package questaoum;
+
+/* Faça um programa em Java que, a partir de um texto digitado pelo usuário, conte o número de caracteres
+total e o número de palavras (palavra é definida por qualquer sequência de caracteres delimitada por espaços em
+branco, não incluir os conectivos, artigos nem os, se, da, dos,) e exiba o resultado. */
+
+import java.util.Scanner;
+
+public class Q01 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		System.out.println("Insira seu texto: ");
+		String texto = teclado.nextLine();
+		String partes [] = texto.split(" ");
+		
+		for (int i = 0; i < partes.length; i++) {
+
+			
+		}
+		System.out.println("O número de caracteres é: ");
+	System.out.println(texto.length());
+	
+	System.out.println("O número de palavras é: ");
+	
+	
+	}
+
+}
+
+
